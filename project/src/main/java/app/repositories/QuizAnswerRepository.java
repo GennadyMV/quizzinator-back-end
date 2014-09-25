5158b87bba5bd18248551da43c791d3a3b8262d9
Aaro Lehikoinen <aaro.lehikoinen@cs.helsinki.fi>
2014-09-25 14:29:17 +0300

    korjattu SQLää, että toimis myös postgresil

--- project/src/main/java/app/repositories/QuizAnswerRepository.java
+++ project/src/main/java/app/repositories/QuizAnswerRepository.java
@@ -10,7 +10,11 @@ import org.springframework.data.repository.query.Param;
 
 public interface QuizAnswerRepository extends JpaRepository<QuizAnswer, Long> {
     public List<QuizAnswer> findByQuizAndUserNot(Quiz quiz, String user, Pageable pageable);
-    @Query("select qa from QuizAnswer qa left join qa.peerReviews where qa.quiz = :quiz and qa.user != :user group by qa order by count(1) asc")
+    @Query("select qa from QuizAnswer qa "+
+            "left join qa.peerReviews "+
+            "where qa.quiz = :quiz and qa.user != :user "+
+            "group by qa.id, qa.answer, qa.ip, qa.quiz, qa.url, qa.user " +
+            "order by count(1) asc")
     public List<QuizAnswer> findByQuizAndUserNotOrderedByReviewCount(@Param("quiz") Quiz quiz, @Param("user") String user, Pageable pageable);
     public List<QuizAnswer> findByQuiz(Quiz quiz);
     public List<QuizAnswer> findByQuizAndUser(Quiz q, String user);
