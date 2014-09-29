1e2ca5e3036f2dcded16d358ee4daf3bdff62cef
Kaltsoon <Kalle.ilves@helsinki.fi>
2014-09-29 13:41:07 +0300

    apin muutos

--- project/src/main/webapp/assets/js/app/services/quiz_api_service.js
+++ project/src/main/webapp/assets/js/app/services/quiz_api_service.js
@@ -33,19 +33,13 @@ QuizApp.service('QuizAPI', ['$http', function($http){
 	_public.edit_quiz = function(options){
 		options.quiz.items = angular.toJson(options.quiz.items);
 
-		var id = options.quiz.id;
-		var quiz = {
-			title: options.quiz.title,
-			items: options.quiz.items
-		}
-
 		$http({
 			method: 'POST',
-			url: 'quiz/' + id,
+			url: 'quiz/' + options.quiz.id,
 			headers: {
 				'Content-Type': 'application/json'
 			},
-			data: angular.toJson(quiz)
+			data: angular.toJson(options.quiz)
 		})
 		.success(function(){
 			options.success();
