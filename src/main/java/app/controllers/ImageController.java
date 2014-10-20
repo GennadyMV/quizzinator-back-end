4039230db0525e2b723b15d7453d26c345d2249f
Niko Kortstrom <niko.kortstrom@helsinki.fi>
2014-10-20 15:51:12 +0300

    changed post image return

--- src/main/java/app/controllers/ImageController.java
+++ src/main/java/app/controllers/ImageController.java
@@ -1,12 +1,8 @@
 package app.controllers;
 
 import app.services.ImageService;
-import java.awt.Image;
 import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -33,7 +29,7 @@ public class ImageController {
             !file.getContentType().equals("image/jpeg") &&
             !file.getContentType().equals("image/png")) {
             
-            return null;
+            return "";
         }
         
         return "images/" + imageService.saveImage(file);
