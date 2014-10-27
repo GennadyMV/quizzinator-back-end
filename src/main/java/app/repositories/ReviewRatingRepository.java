bf70140b3d028710ec5a5fcff4d1c03a03198f73
Niko Kortstrom <niko.kortstrom@helsinki.fi>
2014-10-27 16:47:24 +0200

    added reviewRatings too be included in point service

--- src/main/java/app/repositories/ReviewRatingRepository.java
+++ src/main/java/app/repositories/ReviewRatingRepository.java
@@ -1,7 +1,12 @@
 package app.repositories;
 
+import app.domain.PeerReview;
 import app.domain.ReviewRating;
+import app.domain.User;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReviewRatingRepository extends JpaRepository<ReviewRating, Long>  {
+    List<ReviewRating> findByRater(User rater);
+    List<ReviewRating> findByReview(PeerReview peerReview);
 }
--- src/main/java/app/services/PointService.java
+++ src/main/java/app/services/PointService.java
@@ -3,12 +3,14 @@ package app.services;
 import app.domain.PeerReview;
 import app.domain.Quiz;
 import app.domain.QuizAnswer;
+import app.domain.ReviewRating;
 import app.models.QuizPointModel;
 import app.domain.User;
 import app.models.UserPointModel;
 import app.repositories.PeerReviewRepository;
 import app.repositories.QuizAnswerRepository;
 import app.repositories.QuizRepository;
+import app.repositories.ReviewRatingRepository;
 import app.repositories.UserRepository;
 import java.util.ArrayList;
 import java.util.List;
@@ -30,14 +32,16 @@ public class PointService {
     @Autowired
     private UserRepository userRepo;
     
+    @Autowired
+    private ReviewRatingRepository ratingRepo;
+    
     public UserPointModel getPointsForUser(String id) {
         User user = userRepo.findOne(id);
         
-        return null;
-//        return new UserPointModel(user.getName(),
-//                                answerRepo.findByUser(user).size(),
-//                                reviewRepo.findByReviewer(user).size(),
-//                                ratingRepo.findByRater(user).size());
+        return new UserPointModel(user.getName(),
+                                answerRepo.findByUser(user).size(),
+                                reviewRepo.findByReviewer(user).size(),
+                                ratingRepo.findByRater(user).size());
     }
     
     public QuizPointModel getPointsForQuiz(Long id) {
@@ -46,30 +50,29 @@ public class PointService {
         List<String> reviewers = new ArrayList<String>();
         List<String> raters = new ArrayList<String>();
         
-        return null;
-//        List<QuizAnswer> quizAnswers = quiz.getQuizAnswers();
-//        for (int i = 0; i < quizAnswers.size(); i++) {
-//            QuizAnswer answer = quizAnswers.get(i);
-//            
-//            if (!answerers.contains(answer.getUser().getName())) {
-//                answerers.add(answer.getUser().getName());
-//            }
-//            
-//            List<PeerReview> reviews = answer.getPeerReviews();
-//            for (int j = 0; j < reviews.size(); j++) {
-//                if (!reviewers.contains(reviews.get(j).getReviewer().getName())) {
-//                    reviewers.add(reviews.get(j).getReviewer().getName());
-//                }
-//                
-//                List<ReviewRating> ratings = ratingRepo.findByPeerReview(reviews.get(j));
-//                for (int k = 0; k < ratings.size(); k++) {
-//                    if (!raters.contains(ratings.get(k).getRater().getName())) {
-//                        raters.add(ratings.get(k).getRater().getName());
-//                    }
-//                }
-//            }
-//        }
-//        
-//        return new QuizPointModel(quiz, answerers, reviewers, raters);
+        List<QuizAnswer> quizAnswers = quiz.getQuizAnswers();
+        for (int i = 0; i < quizAnswers.size(); i++) {
+            QuizAnswer answer = quizAnswers.get(i);
+            
+            if (!answerers.contains(answer.getUser().getName())) {
+                answerers.add(answer.getUser().getName());
+            }
+            
+            List<PeerReview> reviews = answer.getPeerReviews();
+            for (int j = 0; j < reviews.size(); j++) {
+                if (!reviewers.contains(reviews.get(j).getReviewer().getName())) {
+                    reviewers.add(reviews.get(j).getReviewer().getName());
+                }
+                
+                List<ReviewRating> ratings = ratingRepo.findByReview(reviews.get(j));
+                for (int k = 0; k < ratings.size(); k++) {
+                    if (!raters.contains(ratings.get(k).getRater().getName())) {
+                        raters.add(ratings.get(k).getRater().getName());
+                    }
+                }
+            }
+        }
+        
+        return new QuizPointModel(quiz, answerers, reviewers, raters);
     }
 }
