6af778940b8ede7cf0fbbfa1b03167155fd3c3b0
Aaro Lehikoinen <aaro.lehikoinen@cs.helsinki.fi>
2014-11-03 13:42:56 +0200

    lib-koodi on nyt saatavilla share-nappulan takaa

--- src/main/webapp/assets/js/app/directives/share_libs.js
+++ src/main/webapp/assets/js/app/directives/share_libs.js
@@ -4,17 +4,12 @@ QuizApp.directive('shareLibs', function () {
             apiurl: '=ngModel'
         },
         link: function (scope, elem, attrs) {
-            $(elem).html('Add this stylesheet tag\
-      <pre>&lt;link rel=&quot;stylesheet&quot; href=&quot;' + scope.apiurl + 'assets/css/quiz-plugin.min.css&quot;&gt;</pre>\
-\
-      and this script tag\
-      <pre>&lt;script src=&quot;' + scope.apiurl + 'assets/js/quiz-plugin.min.js&quot;&gt;&lt;/script&gt;</pre>\
-        \
-        under the head-element of your web page.');
+            $(elem).html('<pre></pre>');
 
-            $(elem).find('.select-quiz-share-code').on('click', function () {
-                $(elem).find('textarea').select();
-            })
+            var code = '<link rel="stylesheet" href="' + scope.apiurl + 'assets/css/quiz-plugin.min.css">\n' +
+                        '<script src="' + scope.apiurl + 'assets/js/quiz-plugin.min.js"></script>';
+                
+            $(elem).children().first().text(code);
         }
     }
 });
