6e938bf8733ed1a5d36223e0dd900bff4e519444
Aaro Lehikoinen <aaro.lehikoinen@cs.helsinki.fi>
2014-11-06 16:08:54 +0200

    scopen apiurl muuttujassa oli ylimääräinen kauttaviiva, korjattu

--- src/main/webapp/assets/js/app/controllers/share_quiz_controller.js
+++ src/main/webapp/assets/js/app/controllers/share_quiz_controller.js
@@ -1,4 +1,4 @@
 QuizApp.controller('ShareQuizController', ['$scope', '$routeParams', '$location', function($scope, $routeParams, $location){
 	$scope.quiz_id = $routeParams.quizId;
-        $scope.apiurl = $location.absUrl().split('#')[0];
+        $scope.apiurl = $location.absUrl().split('#')[0].slice(0, -1);
 }]);
